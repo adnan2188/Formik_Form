0a31993b5e38a6f3dd0a0f71cad20d431de10049
UkkashAhmad <ahmadukkash47@gmail.com>
2024-02-16 15:01:04 +0500

    added backend

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,122 +1,168 @@
-import * as Yup from 'yup';
-import { Formik } from 'formik';
-import './Register.css';
-import { Link } from 'react-router-dom';
+import * as Yup from "yup";
+import { Formik } from "formik";
+import "./Register.css";
+import { Link } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
-
-// alert 
-import toast, { Toaster } from 'react-hot-toast';
+import toast from "react-hot-toast";
 
 // Schema
 const registerSchema = Yup.object({
-    name: Yup.string().min(3).max(25).required("Please enter your name !"),
-    email: Yup.string().email("Invalid email address").required("Please enter your eimail !"),
-    password: Yup.string().min(6).required("Please enter your password !"),
-    confirm_password: Yup.string().required("Please confirm your password !").oneOf([Yup.ref('password'), null], "Password must match"),
-})
-
+  username: Yup.string().min(3).max(25).required("Please enter your name !"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Please enter your eimail !"),
+  password: Yup.string().min(6).required("Please enter your password "),
+  confirm_password: Yup.string()
+    .required("Please confirm your password !")
+    .oneOf([Yup.ref("password"), null], "Password must match"),
+});
 
 // Define the initial values for the form fields
 const initialValuesRegister = {
-    name: "",
-    email: "",
-    password: "",
-    confirm_password: "",
-}
-const notify = () => toast('registration successfully 😊');
+  username: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+};
+
 // Create a functional component named Form
 const Register = () => {
-    const handleFormSubmit = async (values, onSubmitProps) => {
-        console.log(values)
-        onSubmitProps.resetForm();
-        // Show a toast notification for alert
-        notify();
-    };
-    // Return the JSX representing the form
-    return (
-        <>
-            <Toaster />
+  const register = async (values, onSubmitProps) => {
+    const promise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        resolve();
+      } else {
+        reject();
+      }
+    });
+    await toast.promise(promise, {
+        loading:"Registring user ...",
+        success:"Register successfull",
+        error:"error"
+    })
+    
+    onSubmitProps.resetForm();
+  };
 
-            <div className='container'>
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    await register(values, onSubmitProps);
+    // Show a toast notification for alert
+  };
+  // Return the JSX representing the form
+  return (
+    <>
+      <div className="container">
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValuesRegister}
+          validationSchema={registerSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            handleBlur,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="go_back-dvi">
+                <Link to="/">
+                  <IoClose style={{ color: "black", fontSize: "25px" }} />
+                </Link>
+              </div>
+              <h1>Register</h1>
+              <div className="contact_form-div">
+                <label htmlFor="">Name</label>
+                <input
+                  type="text"
+                  autoComplete="off"
+                  name="username"
+                  id="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="enter you name"
+                />
+              </div>
+              {errors.username && touched.username ? <p>{errors.username}</p> : null}
+              <div className="contact_form-div">
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  autoComplete="off"
+                  id="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="example@gmail.com"
+                />
+              </div>
+              {errors.email && touched.email ? <p>{errors.email}</p> : null}
+              <div className="contact_form-div">
+                <label htmlFor="">Password </label>
+                <input
+                  type="password"
+                  autoComplete="off"
+                  id="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="enter your password"
+                />
+              </div>
+              {errors.password && touched.password ? (
+                <p>{errors.password}</p>
+              ) : null}
+              <div className="contact_form-div">
+                <label htmlFor="">Confirm Password </label>
+                <input
+                  type="password"
+                  autoComplete="off"
+                  name="confirm_password"
+                  id="conirm_password"
+                  value={values.confirm_password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="confirm your password"
+                />
+              </div>
+              {errors.confirm_password && touched.confirm_password ? (
+                <p>{errors.confirm_password}</p>
+              ) : null}
 
-                <Formik
-                    onSubmit={handleFormSubmit}
-                    initialValues={initialValuesRegister}
-                    validationSchema={registerSchema}
+              <div>
+                <button type="submit">Submit</button>
+                <h5
+                  style={{
+                    textAlign: "center",
+                    margin: "0px",
+                    marginTop: "30px",
+                  }}
                 >
-
-                    {({ values, errors, touched, handleChange, handleSubmit, handleBlur }) => (
-
-                        <form onSubmit={handleSubmit} >
-                            <div className='go_back-dvi'><Link to="/"><IoClose style={{ color: "black", fontSize: "25px" }} /></Link></div>
-                            <h1>Register</h1>
-                            <div className="contact_form-div">
-                                <label htmlFor="">Name</label>
-                                <input
-                                    type="name"
-                                    autoComplete="off"
-                                    name="name"
-                                    id="name"
-                                    value={values.name}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder='enter you name'
-                                />
-                            </div>
-                            {errors.name && touched.name ? (<p>{errors.name}</p>) : null}
-                            <div className="contact_form-div">
-                                <label htmlFor="">Email</label>
-                                <input
-                                    type="email"
-                                    autoComplete="off"
-                                    id="email"
-                                    name="email"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder='example@gmail.com'
-                                />
-                            </div>
-                            {errors.email && touched.email ? (<p>{errors.email}</p>) : null}
-                            <div className="contact_form-div">
-                                <label htmlFor="">Password </label>
-                                <input
-                                    type="password"
-                                    autoComplete="off"
-                                    id="password"
-                                    name="password"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder='enter your password'
-                                />
-                            </div>
-                            {errors.password && touched.password ? (<p>{errors.password}</p>) : null}
-                            <div className="contact_form-div">
-                                <label htmlFor="">Confirm Password </label>
-                                <input
-                                    type="password"
-                                    autoComplete="off"
-                                    name="confirm_password"
-                                    id='conirm_password'
-                                    value={values.confirm_password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder='confirm your password'
-                                />
-                            </div>
-                            {errors.confirm_password && touched.confirm_password ? (<p>{errors.confirm_password}</p>) : null}
-
-                            <div>
-                                <button type='submit'>Submit</button>
-                                <h5 style={{ textAlign: "center", margin: "0px", marginTop: "30px" }}>if you have allready an account <Link style={{ color: "blue" }} to="/login">Login</Link></h5>
-                            </div>
-                        </form>
-                    )}
-                </Formik>
-            </div>
-        </>
-    )
-}
+                  if you have allready an account{" "}
+                  <Link style={{ color: "blue" }} to="/login">
+                    Login
+                  </Link>
+                </h5>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
+};
 
 export default Register;
