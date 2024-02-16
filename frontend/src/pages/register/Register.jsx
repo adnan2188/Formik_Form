40efce50d7873c1b8b2e8fce5f48b030a327a8d2
UkkashAhmad <ahmadukkash47@gmail.com>
2024-02-16 14:38:34 +0500

    asd

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -11,7 +11,7 @@ const registerSchema = Yup.object({
   email: Yup.string()
     .email("Invalid email address")
     .required("Please enter your eimail !"),
-  password: Yup.string().min(6).required("Please enter your password !"),
+  password: Yup.string().min(6).required("Please enter your password "),
   confirm_password: Yup.string()
     .required("Please confirm your password !")
     .oneOf([Yup.ref("password"), null], "Password must match"),
