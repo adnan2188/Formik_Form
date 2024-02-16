2525ddbab63a71c1491e109bda62db2af2fc4e82
adnan2188 <madnan2188@gmail.com>
2024-02-16 14:47:17 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Register from './/pages/register/Register'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import About from './pages/about/About'
+import Services from './pages/services/Services'
+import Projects from './pages/projects/Projects'
+import Contact from './pages/contact/Contact'
 const App = () => {
   return (
     <div>
@@ -13,6 +18,10 @@ const App = () => {
           <Route path='/register' element={<Register />} />
           <Route path='/*' exact element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
